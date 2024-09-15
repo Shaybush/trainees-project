031a96a65d96c7e6951a7a0e142bd6a752d41b2a
Shay Bushary <shaybush93@gmail.com>
2024-09-15 21:05:24 +0300

    renaming

--- src/app/views/analysis-view/filters/analysis-chart-filters.ts
+++ src/app/views/analysis-view/filters/analysis-chart-filters.ts
@@ -1,19 +1,5 @@
-import {
-  IAnalysisChartDataModel,
-  IAnalysisFilterOptionsModel,
-} from '../models/i-analysis-view.model';
+import { IAnalysisChartDataModel } from '../models/i-analysis-view.model';
 import { IStudentElementModel } from '../../../shared/models/i-student-data.model';
-
-// todo - [spike] check if can be implement with filter service
-/**
- * filter over the time means all the users with their avg grades
- * */
-// export function filterOverTimeChartData(
-//   students: IStudentElementModel[],
-//   filterOptions?: IAnalysisFilterOptionsModel,
-// ): IAnalysisChartDataModel[] {
-//   return [{ label: '', value: 15 }];
-// }
 
 /**
  * filter per subject means all the chosen subject and the average in each subject
@@ -25,27 +11,28 @@ export function filterPerSubjectChartData(students: IStudentElementModel[], filt
     subjects = filteredSubjects.map(subject => subject.toLowerCase());
   }
 
-  const result = students.reduce(
-    (acc, student) => {
-      const { subject, grade } = student;
-      const key = subject.toLowerCase();
+  const subjectGradeSummary = students.reduce((subjectGradesMap, student) => {
+    const { subject, grade } = student;
+    const subjectKey = subject.toLowerCase();
 
-      // Check if subjects exist and filter them
-      if (!subjects || (subjects && subjects.includes(key))) {
-        if (!acc[key]) acc[key] = { subject, totalGrades: 0, exams: 0 };
-        acc[key].totalGrades += grade;
-        acc[key].exams += 1;
+    // Check if subjects exist and filter them
+    if (!subjects || (subjects && subjects.includes(subjectKey))) {
+      if (!subjectGradesMap[subjectKey]) {
+        subjectGradesMap[subjectKey] = { subject, totalGrades: 0, examCount: 0 };
       }
-      return acc;
-    },
-    {} as Record<string, { subject: string; totalGrades: number; exams: number }
-    >,
+      subjectGradesMap[subjectKey].totalGrades += grade;
+      subjectGradesMap[subjectKey].examCount += 1;
+    }
+    return subjectGradesMap;
+  },
+    {} as Record<string, { subject: string; totalGrades: number; examCount: number }>
   );
 
-  return Object.values(result).map(item => ({
+  return Object.values(subjectGradeSummary).map(item => ({
     label: item.subject,
-    value: Math.floor(item.totalGrades / item.exams),
+    value: Math.floor(item.totalGrades / item.examCount),
   }));
+
 }
 
 /**
@@ -61,24 +48,23 @@ export function filterStudentAvgByIdChartData(
     ids = filterIds.map(id => id);
   }
 
-  const result = students.reduce(
-    (acc, student) => {
-      const { name, grade, id } = student;
-      const key = name.toLowerCase();
-      if (!ids || (ids && ids.includes(id))) {
-        if (!acc[key]) acc[key] = { name, totalGrades: 0, exams: 0 };
+  const studentGradeSummary = students.reduce((studentGradesMap, student) => {
+    const { name, grade, id } = student;
+    const studentKey = name.toLowerCase();
 
-        acc[key].totalGrades += grade;
-        acc[key].exams += 1;
+    if (!ids || (ids && ids.includes(id))) {
+      if (!studentGradesMap[studentKey]) {
+        studentGradesMap[studentKey] = { name, totalGrades: 0, examCount: 0 };
       }
+      studentGradesMap[studentKey].totalGrades += grade;
+      studentGradesMap[studentKey].examCount += 1;
+    }
 
-      return acc;
-    },
-    {} as Record<string, { name: string; totalGrades: number; exams: number }>,
-  );
+    return studentGradesMap;
+  }, {} as Record<string, { name: string; totalGrades: number; examCount: number }>);
 
-  return Object.values(result).map(item => ({
+  return Object.values(studentGradeSummary).map(item => ({
     label: item.name,
-    value: Math.floor(item.totalGrades / item.exams),
+    value: Math.floor(item.totalGrades / item.examCount),
   }));
 }
